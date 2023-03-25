9b7008e670ef9257ea14f9ef7e5522d274a730db
James X <xtremejames1@gmail.com>
2023-03-24 20:38:03 -0500

    Added code for simia_core.c for shift register limit switches, still need to implement fully, cleaned up simia_core_map.h

--- simia_core_map.h
+++ simia_core_map.h
@@ -42,27 +42,19 @@
 
 // Define stepper driver enable/disable output pin.
 #define ENABLE_PORT             GPIO_OUTPUT
-#define STEPPERS_ENABLE_PIN     10
+#define STEPPERS_ENABLE_PIN     14
 
 // Define homing/hard limit switch input pins.
-#define X_LIMIT_PIN             11
-#define Y_LIMIT_PIN             12
-#define Z_LIMIT_PIN             13
+#define X_LIMIT_PIN             18
+#define Y_LIMIT_PIN             19
+#define Z_LIMIT_PIN             20
 #define LIMIT_INMODE            GPIO_MAP
 
 // Define ganged axis or A axis step pulse and step direction output pins.
-#if N_ABC_MOTORS > 0
 #define M3_AVAILABLE
 #define M3_STEP_PIN             (STEP_PINS_BASE + 3)
 #define M3_DIRECTION_PIN        (Z_DIRECTION_PIN + 1)
 #define M3_LIMIT_PIN            (Z_LIMIT_PIN + 1)
-#else
-#define AUXINPUT0_PIN           14
-#define AUXOUTPUT0_PORT         GPIO_OUTPUT
-#define AUXOUTPUT0_PIN          5
-#define AUXOUTPUT1_PORT         GPIO_OUTPUT
-#define AUXOUTPUT1_PIN          9
-#endif
 
 // Define spindle enable and spindle direction output pins.
 #define SPINDLE_PORT            GPIO_OUTPUT
@@ -79,9 +71,9 @@
 #define COOLANT_MIST_PIN        17
 
 // Define user-control controls (cycle start, reset, feed hold) input pins.
-#define RESET_PIN               18
-#define FEED_HOLD_PIN           19
-#define CYCLE_START_PIN         20
+#define RESET_PIN               16
+#define FEED_HOLD_PIN           1
+#define CYCLE_START_PIN         0
 
 //Define SD card pins
 #if SDCARD_ENABLE
